1f0c8b5da9f06dfafdbbc128484c60b4eb666316
waadosama <waadosama03@gmail.com>
2023-07-13 16:28:52 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Description: A C program that prints with printf function
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {       
 printf("with proper grammar, but the outcome is a piece of art,\n ");
