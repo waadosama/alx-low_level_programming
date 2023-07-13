6d65ab0c7c7f4f8af5edd925d908f78dbeafb9cb
waadosama <waadosama03@gmail.com>
2023-07-13 16:20:09 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
  * main - Entry point 
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-	Puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+Puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+
 }
 
