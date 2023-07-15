2012e5470608e7c076cc69599c0d9a13751ba359
waadosama <waadosama03@gmail.com>
2023-07-14 23:15:25 -0700

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print 0,1-9
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit  = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+			putchar(',');
+			putchar(' ');
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
