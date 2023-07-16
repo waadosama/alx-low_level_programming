ca94d904c6abd19e354a31e44949165e2cc69e7f
waadosama <waadosama03@gmail.com>
2023-07-15 18:29:17 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,8 @@ while (digit2 <= 9)
 {
 if (digit1 != digit2 && digit1 < digit2)
 {
-putchar(digit1 + 48);
-putchar(digit2 + 48);
+putchar(digit1 + 48)
+putchar(digit2 + 48)
 if (digit1 + digit2 != 17)
 {
 putchar(",");
