36d5e1bc58f0e635381f4fa5a750b011bbec3f6b
waadosama <waadosama03@gmail.com>
2023-07-15 17:36:07 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,19 @@
  * Return: 0 (success)
  */
 int main(void)
-{int digit1 = 0, digit2;
+{
+int digit1 = 0, digit2;
 while (digit1 <= 9)
 {digit2 = 0;
 while (digit2 <= 9)
 {
 if (digit1 != digit2 && digit1 < digit2)
-{putchar(digit1 + 48);
+{
+putchar(digit1 + 48);
 putchar(digit2 + 48);
 if (digit1 + digit2 != 17)
-{putchar(",");
+{
+putchar(",");
 putchar(' ');
 }
 }
