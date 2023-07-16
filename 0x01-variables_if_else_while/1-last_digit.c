65405d9d9e4b4da5388706613deb742c787d884a
waadosama <waadosama03@gmail.com>
2023-07-15 21:30:12 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 else if (digit < 6 && digit != 0)
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 else if (digit == 0)
-printf("last digit of %d is %d and is 0\n", n, digit);
+printf("Last digit of %d is %d and is 0\n", n, digit);
 return (0);
 }
