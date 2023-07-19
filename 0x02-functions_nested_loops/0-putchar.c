2ea49bd626bd40d6f2ab64d03a419acbb196ef27
waadosama <waadosama03@gmail.com>
2023-07-18 17:12:44 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+
+for(ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}	
