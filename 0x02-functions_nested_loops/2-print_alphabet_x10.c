17a5b877b31b47328c9dd8ccb64d348459930613
waadosama <waadosama03@gmail.com>
2023-07-19 19:45:19 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,23 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
-
 void print_alphabet_x10(void)
 {
-int line, ch;
-for (line = 0; line <= 9; line++)
-{
-for (ch = 'a'; ch = 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
-}
+
