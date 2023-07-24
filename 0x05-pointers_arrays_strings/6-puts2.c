c47408a313672e3cf8ca59d17df9ecdb763eac60
waadosama <waadosama03@gmail.com>
2023-07-24 16:29:24 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 -  prints every character of a string
+ *@str : string parmater input
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
