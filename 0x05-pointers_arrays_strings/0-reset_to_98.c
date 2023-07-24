e22cfe3312000393b7598a8e674b6c4b677b059d
waadosama <waadosama03@gmail.com>
2023-07-24 14:35:34 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * reset_to_98 - takes a pointer to an int paramter
+ *               and updatesthe value it point to 98
+ *
+ * @n:int paramter
+ *
+ * Return: Nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
