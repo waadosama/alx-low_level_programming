a9a07c58753754714de1215e9c81fd4b6217ee43
waadosama <waadosama03@gmail.com>
2023-07-25 18:20:29 -0700

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src
+ *@dest: char type string
+ * @src: char type string
+ * description : copy the string pointed to by pointers 'src'to
+ * the buffer pointed to by 'dest'
+ * Return: pointer to 'dest'
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = -i;
+
+do {
+i++;
+dest[i] = scr[i];
+} while (scr[i] != '\0');
+return (dest);
+}
