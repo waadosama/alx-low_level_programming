aea246abaedd8e494ea8e7f95a84afb0d3c3268a
waadosama <waadosama03@gmail.com>
2023-07-25 19:56:41 -0700

    a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an intger
+ *
+ * @s: string input paramter
+ *
+ * Return : converted integer from string
+*/
+int _atoi(char *s)
+{unsigned int num = 0;
+int sign = 1;
+
+do {
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+num = (num * 10) + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *sign);
+}
