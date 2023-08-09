8e002a1a00471f104c436d64c602509478e3f868
waadosama <waadosama03@gmail.com>
2023-08-09 14:41:42 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,11 +4,11 @@
  * @m: string
  * Return:int
 */
-int _strlen(char *m)
+int _strlen(char *s)
 {
 int size = 0;
 
-for (; m[size] != '\0'; size++)
+for (; s[size] != '\0'; size++)
 return (size);
 }
 /**
