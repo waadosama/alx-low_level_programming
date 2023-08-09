de9d07edd58ccf261d30fda8e4295a515945cad2
waadosama <waadosama03@gmail.com>
2023-08-09 14:36:50 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@ int _strlen(char *m)
 {
 int size = 0;
 
-for (; s[size] != '\0'; size++)
+for (; m[size] != '\0'; size++)
 return (size);
 }
 /**
@@ -22,9 +22,9 @@ char *str_concat(char *s1, char *s2)
 int size1, size2, i;
 char *m;
 if (s1 == NULL)
-s1 = "\0"
+s1 = "\0";
 if (s2 == NULL)
-s2 = "\0"
+s2 = "\0";
 size1 = _strlen(s1);
 size2 = _strlen(s2);
 m = malloc((size1 + size2)*sizeof(char) +  1);
