89a60ec26bc7062da3f6cc6c7098587f0cf02dbc
waadosama <waadosama03@gmail.com>
2023-08-09 15:52:51 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the number of argumnent passed  into program
+ * @argc : int
+ * @argv : list
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+int i = 0;
+
+while (argc--)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
