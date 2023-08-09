f066d7caa9f513e2e9aed8563f5425093f74516f
waadosama <waadosama03@gmail.com>
2023-08-09 15:18:37 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print program name , followe byb anew line
+ * @argc : int
+ * @argv : list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
