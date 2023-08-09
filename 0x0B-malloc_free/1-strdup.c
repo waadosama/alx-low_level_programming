5da2098ab740c164e7aca4378096427077cc5edc
waadosama <waadosama03@gmail.com>
2023-08-09 13:24:16 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,7 +10,7 @@ char *_strdup(char *str)
 int i = 0, size = 0;
 char *m;
 
-if (str == NULL)
+if (str == '\0')
 return (NULL);
 for (; str[size] != '\0'; size++)
 m = malloc(size * sizeof(*str) + 1);
