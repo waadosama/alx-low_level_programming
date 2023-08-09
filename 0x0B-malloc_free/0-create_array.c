76ad33f463a617edaf8ac95193b91760793ada6c
waadosama <waadosama03@gmail.com>
2023-08-08 18:49:59 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  *create_array - creats an array of chars
+ * and initializes it with a specfic char
+ * @size : size of array
+ * @c :char to initailize
+ * Return :pointer to array initailized or NuLL
+*/
+char *create_array(unsigned int size, char c)
+{
+char *n = malloc(size);
+
+if (size == 0 || n == 0)
+return (0);
+while (size--)
+n[size] = c;
+return (n);
+}
