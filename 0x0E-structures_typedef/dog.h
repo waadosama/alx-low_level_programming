b97f77edbc3c6c95a708bb276deb878bb8eed56a
waadosama <waadosama03@gmail.com>
2023-08-14 18:15:00 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name : dog's name
+ * @age : dog's age
+ * @owener : owener's name
+ *
+ * Description : just a long dog struct in big kitty world
+*/
+struct dog
+{
+char *name;
+float *age;
+char *owener;
+};
+#endif
