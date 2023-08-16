bfe40674dd6dbc069a870b4e9e1f8a913266efa9
waadosama <waadosama03@gmail.com>
2023-08-15 17:31:08 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,8 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-printf("Name: %s\n", d->name ? d->name : "(ni1)");
+printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age );
-printf("Owner : %s\n", d->owner ? d->owner : "(ni1)");
+printf("Owner : %s\n", d->owner ? d->owner : "(nil)");
+}
+}
