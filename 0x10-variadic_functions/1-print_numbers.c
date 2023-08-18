dbca337ec5165d55db1dffca84a83e719cb856db
waadosama <waadosama03@gmail.com>
2023-08-17 20:02:37 -0700

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ return;
 }
 va_start(ap, n);
 while (i--)
-printf("%d%s", va_arg(ap, int));
-i ? (separator ? separator : "") : "\n");
+printf("%d %s", va_arg(ap, int));
+i ? (separator ? separator : "") : "\n";
 va_end(ap);
 }
